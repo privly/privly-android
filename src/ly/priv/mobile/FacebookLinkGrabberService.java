56f0379dcb203a5dd0bf0439ddabd5502b26bf01
ivanmetla <metlaivan@gmail.com>
2014-05-25 22:12:12 +0300

    add login and logout to FaceBook

--- src/ly/priv/mobile/FacebookLinkGrabberService.java
+++ src/ly/priv/mobile/FacebookLinkGrabberService.java
@@ -1,5 +1,7 @@
 package ly.priv.mobile;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -15,10 +17,15 @@ import org.json.JSONObject;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -68,6 +75,23 @@ public class FacebookLinkGrabberService extends SherlockFragment {
 
 		super.onCreateView(inflater, container, savedInstanceState);
 		View view = inflater.inflate(R.layout.link_grabber_service, container, false);
+		// Add code to print out the key hash
+	    try {
+	        PackageInfo info = getActivity().getPackageManager().getPackageInfo(
+	                "com.facebook.samples.hellofacebook", 
+	                PackageManager.GET_SIGNATURES);
+	        for (Signature signature : info.signatures) {
+	            MessageDigest md = MessageDigest.getInstance("SHA");
+	            md.update(signature.toByteArray());
+	            Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+	            }
+	    } catch (NameNotFoundException e) {
+
+	    } catch (NoSuchAlgorithmException e) {
+
+	    }
+		
+		
 		context = getActivity();
 		current = this;
 		Log.d("OnCreateView", "OnCreateView");
--- src/ly/priv/mobile/gui/socialnetworks/SListUsersActivity.java
+++ src/ly/priv/mobile/gui/socialnetworks/SListUsersActivity.java
@@ -2,12 +2,14 @@ package ly.priv.mobile.gui.socialnetworks;
 
 import java.util.ArrayList;
 
+import ly.priv.mobile.Login;
 import ly.priv.mobile.R;
-
-import com.actionbarsherlock.app.SherlockFragment;
-
+import ly.priv.mobile.Settings;
+import ly.priv.mobile.Values;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,58 +17,133 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockFragment;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
+import com.facebook.Session;
+import com.facebook.SessionLoginBehavior;
+
 public class SListUsersActivity extends SherlockFragment {
+	private static final String TAG = "SListUsersActivity";
 	private ArrayList<SUser> mListUserMess;
 	private ListUsersAdapter mListUserMessagesAdapter;
 	private ListView mListViewUsers;
+	private Session mSession;
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.activity_list, container, false);
+		setHasOptionsMenu(true);
+		ActionBar actionBar = getSherlockActivity().getSupportActionBar();
+		actionBar.setTitle(R.string.privly_Facebook);
 		this.mListViewUsers = ((ListView) view.findViewById(R.id.lView));
-	
-		ArrayList<SMessage> listSMessage = new ArrayList<SMessage>();
-		mListUserMess = new ArrayList<SUser>();
-		listSMessage.add(new SMessage("hid", "13.11.1991", "", true));
-		listSMessage.add(new SMessage("hid", "13.11.1991", "", false));
-		listSMessage.add(new SMessage("hid", "13.11.1991", "", false));
-		listSMessage.add(new SMessage("hid", "13.11.1991", "", true));
-		mListUserMess.add(new SUser("name",
-				"hi. What are you doing", "13.11.2014", "",
-				listSMessage));
-		mListUserMess.add(new SUser("name1",
-				"hi. What are you doing", "13.01.2014", "",
-				listSMessage));
-		mListUserMess.add(new SUser("name2",
-				"hi. What are you doing", "13.12.2014", "",
-				listSMessage));
-			if (this.mListUserMess != null) {
-				this.mListUserMessagesAdapter = new ListUsersAdapter(getActivity(),
-						this.mListUserMess);
-				this.mListViewUsers.setAdapter(this.mListUserMessagesAdapter);
-			}
-	
+		mSession = Session.getActiveSession();
+		login();
+
+		if (this.mListUserMess != null) {
+			this.mListUserMessagesAdapter = new ListUsersAdapter(getActivity(),
+					this.mListUserMess);
+			this.mListViewUsers.setAdapter(this.mListUserMessagesAdapter);
+		}
+
 		mListViewUsers.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
-				FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();		
-				SListUserMessagesActivity sListUserMessagesActivity =new SListUserMessagesActivity();
+				FragmentTransaction transaction = getActivity()
+						.getSupportFragmentManager().beginTransaction();
+				SListUserMessagesActivity sListUserMessagesActivity = new SListUserMessagesActivity();
 				Bundle bundle = new Bundle();
-				bundle.putSerializable("UserMessages", mListUserMess
-						.get(position).getListUserMess());
+				bundle.putSerializable("UserMessages",
+						mListUserMess.get(position).getListUserMess());
 				sListUserMessagesActivity.setArguments(bundle);
 				transaction.replace(R.id.container, sListUserMessagesActivity);
 				transaction.disallowAddToBackStack();
 				transaction.commit();
-			
-				
 
 			}
 		});
 		return view;
-		
+
 	}
 
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		Session.getActiveSession().onActivityResult(getActivity(), requestCode,
+				resultCode, data);
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		Session session = Session.getActiveSession();
+		Session.saveSession(session, outState);
+	}
+
+	/**
+	 * Inflate options menu with the layout
+	 */
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		inflater.inflate(R.layout.menu_layout_slistusers, menu);
+		super.onCreateOptionsMenu(menu, inflater);
+
+	}
+
+	/**
+	 * Item click listener for options menu.
+	 * <p>
+	 * relogin
+	 * </p>
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		switch (item.getItemId()) {
+		case R.id.logout:
+			mSession.closeAndClearTokenInformation();
+			mSession=null;
+			login();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	/**
+	 * Login in FaceBook
+	 */
+	private void login() {
+		if (mSession == null) {
+			mSession = new Session.Builder(getActivity()).build();
+			Session.setActiveSession(mSession);
+			if (!mSession.isOpened()) {
+				ArrayList<String> permissions = new ArrayList<String>();
+				permissions.add("read_mailbox");
+				Session.OpenRequest openRequest = new Session.OpenRequest(this);
+				openRequest
+						.setLoginBehavior(SessionLoginBehavior.SSO_WITH_FALLBACK);
+				openRequest
+						.setRequestCode(Session.DEFAULT_AUTHORIZE_ACTIVITY_CODE);
+				openRequest.setPermissions(permissions);
+				mSession.openForRead(openRequest);
+				if (mSession.isOpened()) {
+					// FetchFbMessages task = new FetchFbMessages();
+					// task.execute();
+				}
+			} else {
+				// FetchFbMessages task = new FetchFbMessages();
+				// task.execute();
+			}
+
+		} else {
+			// FetchFbMessages task = new FetchFbMessages();
+			// task.execute();
+		}
+	}
 }
