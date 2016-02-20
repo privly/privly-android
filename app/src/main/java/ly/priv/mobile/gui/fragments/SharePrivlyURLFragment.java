ef5a598ec3454a6776cef1e70e5f0a0e3b790ab0
Aakash Muttineni <aakash.muttineni@gmail.com>
2016-02-20 15:03:31 +0530

    Update sharing screen messaging. #32

--- app/src/main/java/ly/priv/mobile/gui/fragments/SharePrivlyURLFragment.java
+++ app/src/main/java/ly/priv/mobile/gui/fragments/SharePrivlyURLFragment.java
@@ -1,5 +1,6 @@
 package ly.priv.mobile.gui.fragments;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,6 +17,28 @@ import ly.priv.mobile.R;
 import ly.priv.mobile.utils.ConstantValues;
 
 public class SharePrivlyURLFragment extends Fragment {
+
+    SharePrivlyFragInteractionListener mCallback;
+
+    // MainActivity must implement this interface
+    public interface SharePrivlyFragInteractionListener {
+        public void createMessage();
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        // This makes sure that the container activity has implemented
+        // the callback interface. If not, it throws an exception
+        try {
+            mCallback = (SharePrivlyFragInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement SharePrivlyFragInteractionListener");
+        }
+    }
+    
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_share_privly_url, container, false);
@@ -27,6 +50,7 @@ public class SharePrivlyURLFragment extends Fragment {
         linkify.addLinks(urlTextView, Linkify.ALL);
         Button shareButton = (Button) rootView.findViewById(R.id.share_url_btn);
         Button copyButton = (Button) rootView.findViewById(R.id.copy_url_btn);
+        Button createButton = (Button) rootView.findViewById(R.id.create_msg_btn);
         shareButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,6 +78,12 @@ public class SharePrivlyURLFragment extends Fragment {
                     clipboard.setPrimaryClip(clip);
                 }
                 Toast.makeText(getActivity(), "URL copied", Toast.LENGTH_SHORT).show();
+            }
+        });
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCallback.createMessage();
             }
         });
         return rootView;
